b21ad59141f0c1a8145ac4af0c91dfeaa6901591
Rocket-15 <olashupo.kolade@outlook.com>
2023-07-29 21:59:04 +0100

    js tips updated

--- javascript tips.js
+++ javascript tips.js
@@ -1,2 +1,5 @@
 // if src is set the sript content is ignored 
 // to link a script 
+// We can use a <script> tag to add JavaScript code to a page.
+// The type and language attributes are not required.
+// A script in an external file can be inserted with <script src="path/to/script.js"></script>.
