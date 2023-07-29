132fdf1c25f70903cef759b9ba134b55a5606670
Rocket-15 <olashupo.kolade@outlook.com>
2023-07-29 22:13:39 +0100

    alerts

--- javascript tips.js
+++ javascript tips.js
@@ -3,3 +3,4 @@
 // We can use a <script> tag to add JavaScript code to a page.
 // The type and language attributes are not required.
 // A script in an external file can be inserted with <script src="path/to/script.js"></script>.
+alert(I'm javascript);
